be0181c8e3731a74cd5157533be3dea10c7e1ee7
joaoveasey <jovepedro@gmail.com>
2024-01-10 12:39:00 -0300

    Add project files.

---
+++ Anotacoes/Program.cs
@@ -0,0 +1,39 @@
+﻿//a programação orientada a objetos (POO) é uma abordagem para desenvolvimento de software no qual a estrutura é baseada em objetos que interagem uns com os outros para 
+//realizar uma tarefa, "O mundo é um sistema orientado a objetos / O software e o paradigma da orientação a objetos".
+
+
+//a POO teve avanço a partir de problemas de linguagens procedurais, visto que os programas estavam ficando mais complexos, com a manutenção mais difícil
+//pois antigamente a programação nao era voltada a uma equipe, era apenas um desenvolvedor para fazer tudo. além da integração entre os diversos sistemas ser caótica
+
+//--------benefícios da POO:
+//-uma transição mais intuitiva dos modelos de negócio para implementação
+//-capacidade de manter e implementar mudanças nos programas de forma mais rápida e eficiente
+//-capacidade de criar software a partir de um processo em equipe
+//-capacidade de reutilização de código
+//-melhor integração com sistemas operacionais modernos
+//-capacidade de criar uma interface gráfica intuitiva para os usuários
+
+//--------características da POO:
+//- um OBJETO é uma estrutura que incorpora dados e comportamentos para trabalhar com esses dados 
+//(um objeto é uma instância da classe)
+//a classe é o tipo e o objeto é uma instância desse tipo, exemplo:
+//Classe Produto -> objeto: caderno, livro, celular, mouse,etc..
+
+//--------pilares da POO:
+//- a ABSTRAÇÂO em programação de computadores é uma forma de diminuir a complexidade e tornar o projeto mais eficiente.
+//abstrair é a habilidade de se concentrar nos aspectos essenciais de um contexto qualquer, ignorando características menos importantes, ou seja, são propriedades COMUNS de 
+//um conjunto de objetos.
+
+//- o ENCAPSULAMENTO é o processo no qual o acesso direto aos dados de um objeto não é permitido.
+//ao usar o encapsulamento o código de cada objeto deve controlar apenas o seu próprio estado, isso adiciona segurança à aplicação pelo fato de esconder propriedades dos objetos 
+//do acesso direto criando uma espécie de "caixa preta".
+
+//- a HERANÇA é um recurso da POO que permite que você crie novas classes que reutilizam, estendem e modificam o comportamento definido na classe base.
+//a classe cujo membros são herdados é chamada classe base, e a classe que herda esses membros é chamada de classe derivada.
+//a classe base oferece uma funcionalidade COMUM (dados e comportamentos) e as classes derivadas herdam ou substituem essa funcionalidade
+
+//- o POLIMORFISMO é a habilidade de objetos distintos responderem a mesma mensagem de sua própria maneira.
+//você pode enviar a mensagem, mover para cada objeto semelhante a um veículo e cada um vai se comportar de maneira diferente para atender sua solicitação.
+//quando uma mesma mensagem pode ser processada de diferentes formas temos um exemplo de polimorfismo.
+//usando isso podemos invocar métodos da classe derivada através da classe base em tempo de execução, e, permitir que classes forneçam diferentes implementações de métodos
+//que são chamados com o mesmo nome.
