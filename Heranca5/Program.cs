f1bca8ad9cdcc42ab05e8238e07c5e2fa3aae94d
joaoveasey <jovepedro@gmail.com>
2024-01-12 12:23:40 -0300

    #

--- Heranca5/Program.cs
+++ Heranca5/Program.cs
@@ -1,2 +1,74 @@
 ﻿Console.WriteLine("Herança - Modificador Sealed");
 
+//o modificador sealed impede que outras classes herdem dela 
+
+//sintaxe:
+
+//sealed class MinhaClasseSealed
+//{
+
+//}
+
+//também podemos usar o modificador sealed em métodos ou propriedades que substitui um método ou propriedade virtual em uma classe base.
+//com isso você permite que classes sejam derivadas de sua classe e evita que outros devs que estejam usando suas classes substituam métodos e propriedades virtuais específicos.
+
+Cliente cliente = new();
+
+//cliente.Nome = "José";
+cliente.ExibirNome();
+
+
+Console.ReadKey();
+
+sealed class Pessoa
+{
+    public string Nome { get; set; }
+    public void ExibirNome()
+    {
+        Console.WriteLine($"Meu nome é {Nome}");
+    }
+}
+
+class Cliente// : Pessoa
+{
+    public void ExibirNome()
+    {
+        //Console.WriteLine($"Meu nome é {Nome} e sou cliente");
+    }
+}
+
+class ClasseBase
+{
+    protected virtual void Metodo1()
+    {
+        Console.WriteLine("ClasseBase.Metodo1");
+    }
+    protected virtual void Metodo2()
+    {
+        Console.WriteLine("ClasseBase.Metodo2");
+    }
+}
+
+class Classe1 : ClasseBase
+{
+    sealed protected override void Metodo1()
+    {
+        Console.WriteLine("ClasseBase.Metodo1");
+    }
+    protected override void Metodo2()
+    {
+        Console.WriteLine("ClasseBase.Metodo2");
+    }
+}
+
+class CLasse2 : Classe1
+{
+    //protected override void Metodo1()  ==> nesse caso não podemos sobrescrever esse método por estar como sealed na Classe1
+    //{
+    //    Console.WriteLine("ClasseBase.Metodo1");
+    //}
+    protected override void Metodo2()
+    {
+        Console.WriteLine("ClasseBase.Metodo2");
+    }
+}
---
+++ HerancaExercicio/Program.cs
@@ -0,0 +1,106 @@
+﻿Console.WriteLine("Herança - Exercício");
+
+Cliente clienteContaCorrente = new();
+
+clienteContaCorrente.Id = 1;
+clienteContaCorrente.Nome = "João";
+clienteContaCorrente.Depositar(1000);
+clienteContaCorrente.Sacar(100);
+clienteContaCorrente.ExibirSaldo();
+clienteContaCorrente.Sacar(2000);
+
+ContaPoupanca clienteContaPoupanca = new();
+
+clienteContaPoupanca.Id = 1;
+clienteContaPoupanca.Nome = "José";
+clienteContaPoupanca.Depositar(1000);
+clienteContaPoupanca.Sacar(100);
+clienteContaPoupanca.ExibirSaldo();
+clienteContaPoupanca.Sacar(2000);
+
+
+ContaInvestimento clienteContaInvestimento = new();
+
+clienteContaInvestimento.Id = 3;
+clienteContaInvestimento.Nome = "Joca";
+clienteContaInvestimento.Depositar(1000);
+clienteContaInvestimento.Sacar(100);
+clienteContaInvestimento.ExibirSaldo();
+clienteContaInvestimento.Sacar(2000);
+
+
+Console.ReadKey();
+
+public class Cliente
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public double Saldo = 0;
+    
+    public virtual void Depositar(double valorDeposito)
+    {
+        Saldo += valorDeposito;
+    }
+    public virtual void Sacar(double valorSaque)
+    {
+        Saldo -= valorSaque;
+    }
+    public virtual void ExibirSaldo()
+    {
+        Console.WriteLine($"\nOlá {Nome}, seu saldo atual é R${Saldo}\n");
+    }
+}
+
+
+public class ContaPoupanca : Cliente
+{
+    public override void Depositar(double valorDeposito)
+    {
+        Saldo += valorDeposito;
+        double Juros = Saldo * 0.05;
+        Saldo += Juros;
+
+        Console.WriteLine($"O rendimento de Juros é {Juros} e seu novo Saldo é {Saldo}");
+    }
+    public override void Sacar(double valorSaque)
+    {
+        if (valorSaque > Saldo)
+        {
+            Console.WriteLine($"Você não pode ficar com saldo negativo. Seu saldo atual é {Saldo}");
+        }
+        else
+        {
+            Saldo -= valorSaque;
+            Console.WriteLine($"Você sacou R${valorSaque} da sua conta poupança.\nSeu novo Saldo é R${Saldo}");
+        }
+    }
+}
+
+public class ContaInvestimento : Cliente
+{
+    public override void Depositar(double valorDeposito)
+    {
+        Saldo += valorDeposito;
+        double Juros = Saldo * 0.09;
+        Saldo += Juros;
+
+        Console.WriteLine($"O rendimento de Juros é {Juros} e seu novo Saldo é {Saldo}");
+    }
+    public override void Sacar(double valorSaque)
+    {
+        if (valorSaque > Saldo)
+        {
+            //caso o valor do saque seja maior que o saldo, ocorrerá esse erro..
+            Console.WriteLine($"Você não pode ficar com saldo negativo. Seu saldo atual é {Saldo}");
+        }
+        else
+        {
+            Saldo -= valorSaque;
+            double Imposto = Saldo * 0.01;
+            Saldo -= Imposto;
+
+            Console.WriteLine($"Você sacou R${valorSaque} da sua conta investimento.\nO valor do Imposto foi R${Imposto} e seu novo Saldo é R${Saldo}");
+        }
+    }
+}
+
