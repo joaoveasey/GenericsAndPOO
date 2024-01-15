18b87b886cfb6a279bed79496cc1950f95f4021b
joaoveasey <jovepedro@gmail.com>
2024-01-15 12:46:53 -0300

    #

--- Heranca7/Program.cs
+++ Heranca7/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Heranca7;
+
+Console.WriteLine("Tipos de Herança");
+
+//herança simples: é o tipo de herança que há apenas uma classe base e uma classe derivada
+
+//herança hierárquica: é o tipo de herança que existem várias classes derivadas para uma classe base
+
+//herança multinível: quando uma classe é derivada de outra classe derivada
+
+//herança múltipla: ocorre quando temos uma classe base e uma classe derivada que implementa uma ou mais interfaces
+
+
+Console.ReadKey ();
