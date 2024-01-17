3d70dd3c4eb5ee9d8d9df9a2dac027f52c270bf0
joaoveasey <jovepedro@gmail.com>
2024-01-17 13:04:29 -0300

    #

---
+++ HerancaOperador-Is-As/Program.cs
@@ -0,0 +1,44 @@
+﻿// o operador AS é usado para realizar a conversão de tipos de referência ou tipos anuláveis compatíveis
+// este operador retorna o objeto quando ele é comparável retorna o objeto quando ele é compatível com o tipo dado e retorna null se a conversão não for possível
+// ao realizar um downcasting ele avalia o resultado par null ao invés de lançar uma exceção (InvalidCastException) caso não seja possível realizá-la
+
+
+// o operador IS é um operador booleano, que serve para comparar tipos
+// retorna true se o objeto for do mesmo tipo, caso contrário, retorna false (retorna false para tipos nulos tb)
+
+//------exemplo de uso AS:
+
+Object carroObj = new Carro();
+
+
+var x = carroObj as string; //usando o operador as, se gerar uma exception retornará null
+
+if (x != null)
+{
+    Console.WriteLine("Converte obj em string");
+}
+else
+{
+    Console.WriteLine("Não foi possível converter x é null");
+}
+
+//------exemplo de uso IS:
+
+//Circulo circulo = new Circulo();
+//Forma forma = circulo;
+
+//if(forma is Circulo) //verifica com o operador IS se forma e circulo são compatíveis
+//{
+//  Console.WriteLine("Converteu Forma para Círculo\n");
+//
+//  ((Circulo)forma).PintarCirculo();
+//}
+//else
+//{
+//  Console.WriteLine("Operação de downcasting não pode ser feita");
+//}
+
+Console.ReadKey();
+
+class Carro { }
+
---
+++ ModificadorAbstract/Program.cs
@@ -0,0 +1,38 @@
+﻿// o modificador abstract indica que o recurso que está sendo modificado tem uma implementação ausente ou incompleta
+// pode ser usado com classes, métodos, propriedades, indexadores e eventos
+// ao usar o abstract indicamos que a classe destina-se apenas a ser uma classe base de outras classes, não podendo ser instanciada.
+
+
+using System.ComponentModel;
+
+Console.ReadKey();
+
+public abstract class Forma //a classe base pode possuir métodos comuns e métodos abstratos. / não pode ser instanciada, mas pode ser herdada.
+{
+    public double Lado { get; set; }
+    public double Area { get; set; }
+    public double Perimetro { get; set; }
+
+    //método comum
+    public void Desenhar()
+    { }
+
+    //método abstrato => precisa ser instanciado obrigatoriamente nas classes derivadas de Forma
+    public abstract double CalcularArea();
+    public abstract double CalcularPerimetro();
+}
+
+public class Quadrado : Forma
+{
+    public override double CalcularArea()
+    {
+        this.Area = Lado * Lado;
+        return this.Area;
+    }
+
+    public override double CalcularPerimetro()
+    {
+        this.Perimetro = 4 * Lado;
+        return this.Perimetro;
+    }
+}
