d5296d371ea7dc0ebde66a553329877fd08244b6
joaoveasey <jovepedro@gmail.com>
2024-01-26 11:36:34 -0300

    #

---
+++ Composicao/Program.cs
@@ -0,0 +1,4 @@
+﻿// tipos de relacionamento entre classes 
+
+// 1- relacionamento "É UM" - representado pela herança
+// 2- relacionamento "TEM UM" - representado pela composição e pela agregação
---
+++ HerancaEComposicaoExemplo/Program.cs
@@ -0,0 +1,1 @@
+﻿// exemplo de herança e composição
