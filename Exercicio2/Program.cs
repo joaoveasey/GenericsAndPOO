11ea6e6356e1646fa0db1fc725e712f1c3ebfa71
joaoveasey <jovepedro@gmail.com>
2024-02-02 11:02:37 -0300

    #

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,40 @@
+﻿LivroFotos livroComum = new();
+Console.WriteLine(livroComum.GetNumeroPaginas());
+
+LivroFotos livroComumComPaginas = new(24);
+Console.WriteLine(livroComumComPaginas.GetNumeroPaginas());
+
+SuperLivroFotos superLivroFotos = new();
+//Console.WriteLine(superLivroFotos.GetNumeroPaginas);
+
+Console.ReadKey();
+
+public class LivroFotos
+{
+    private int numPaginas;
+    public int GetNumeroPaginas()
+    {
+        return numPaginas;
+    }
+    public LivroFotos()
+    {
+        numPaginas = 16;
+    }
+    public LivroFotos(int _numPaginas)
+    {
+        numPaginas = _numPaginas;
+    }
+}
+
+public class SuperLivroFotos
+{
+    private int numPaginas;
+    public int GetNumeroPaginas()
+    {
+        return numPaginas;
+    }
+    public SuperLivroFotos()
+    {
+        numPaginas = 64;
+    }
+}
---
+++ Exercicio3/Program.cs
@@ -0,0 +1,62 @@
+﻿//Usando os conceitos de polimorfismo escreva um programa para calcular a área do
+//Quadrado, Triângulo e do Circulo
+//Fórmulas das áreas
+//- Quadrado -> A = lado * lado
+//- Triângulo -> A = (base * altura)/2
+//- Circulo -> A = Pi * raio * raio ou A = π . r²
+
+Quadrado quadrado = new(2);
+quadrado.MostraArea();
+
+Triangulo triangulo = new(4, 6);
+triangulo.MostraArea();
+
+Circulo circulo = new(3);
+circulo.MostraArea();
+
+Console.ReadKey();
+
+public class Forma
+{
+    public int area;
+    public virtual void MostraArea()
+    {
+        Console.WriteLine(area);
+    }
+}
+
+public class Quadrado : Forma
+{
+    public Quadrado(int lado)
+    {
+        area = lado * lado;
+    }
+    public override void MostraArea()
+    {
+        Console.WriteLine(area);
+    }
+}
+
+public class Triangulo : Forma
+{
+    public Triangulo(int _base, int altura)
+    {
+        area = (_base * altura) / 2;
+    }
+    public override void MostraArea()
+    {
+        Console.WriteLine(area);
+    }
+}
+
+public class Circulo : Forma
+{
+    public Circulo(int raio)
+    {
+        area = 3 * raio * raio;
+    }
+    public override void MostraArea()
+    {
+        Console.WriteLine(area);
+    }
+}
