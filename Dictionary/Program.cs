e7beb92044d66e947ce73795aa66fc480b56a817
joaoveasey <jovepedro@gmail.com>
2024-02-06 22:56:06 -0300

    #

---
+++ Dictionary/Program.cs
@@ -0,0 +1,53 @@
+﻿//Add(TKey key, TValue value):
+//adiciona um par de chave/valor ao dicionário.
+
+//Remove(TKey key):
+//remove a entrada correspondente à chave especificada do dicionário, se existir.
+
+//ContainsKey(TKey key):
+//verifica se o dicionário contém uma chave específica.
+
+//ContainsValue(TValue value):
+//verifica se o dicionário contém um valor específico.
+
+//Clear():
+//remove todos os pares chave/valor do dicionário.
+
+//Count:
+//retorna o número de pares chave/valor no dicionário.
+
+//TryGetValue(TKey key, out TValue value):
+//tenta obter o valor associado à chave especificada. Retorna true se a chave existir, false caso contrário.
+
+//Keys:
+//retorna uma coleção contendo as chaves no dicionário.
+
+//Values:
+//retorna uma coleção contendo os valores no dicionário.
+
+//GetEnumerator():
+//retorna um enumerador que itera pelos pares chave/valor no dicionário.
+
+Dictionary <int,int> dic1 = new Dictionary<int, int> ();
+var dic2 = new Dictionary<int ,int> ();
+dic2.Add(2, 200);
+dic2.Add(4, 400);
+dic2.Add(3, 400);
+
+
+var dic3 = new Dictionary<int, int>()
+{
+    {1,100 },
+    {2,200 },
+    {3,200 }
+};
+
+try
+{
+    dic3.Add(2, 232);
+}
+catch(Exception ex)
+{
+    Console.WriteLine (ex.Message);
+    Console.WriteLine (ex.ToString());
+}
---
+++ DictionaryExercicio/Program.cs
@@ -0,0 +1,99 @@
+﻿var alunos = new Dictionary<int, Aluno>()
+{
+    {1, new Aluno("Maria", 7) },
+    {2, new Aluno("Eric", 8) },
+    {3, new Aluno("Ana", 9) },
+    {4, new Aluno("Alex", 6) },
+    {5, new Aluno("Diná", 5) }
+};
+
+ExibirColecao(alunos);
+
+do
+{
+    Console.WriteLine("Informe o código do aluno a ser localizado(99 para sair)");
+    int codigo = Int32.Parse(Console.ReadLine());
+
+    if(codigo == 99) 
+        break;
+
+    var resultado = alunos.ContainsKey(codigo);
+    if (resultado)
+    {
+        Console.WriteLine("Informe a nota (1 a 10)");
+        var nota = Int32.Parse(Console.ReadLine());
+        alunos[codigo].Nota = nota;
+    }
+    else
+    {
+        Console.WriteLine("Aluno não localizado..");
+    }
+
+} while (true);
+
+ExibirColecao(alunos);
+
+Console.WriteLine("Informe o código do aluno a ser removido");
+int cod = Int32.Parse(Console.ReadLine());
+if (alunos.ContainsKey(cod))
+{
+    alunos.Remove(cod);
+    Console.WriteLine("Aluno removido.");
+}
+else
+{
+    Console.WriteLine("Aluno não encontrado..");
+}
+
+ExibirColecao(alunos);
+
+Console.WriteLine("Nova nota: ");
+int novaNota = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Novo nome: ");
+string novoNome = Console.ReadLine();
+
+Console.WriteLine("Informe o código do aluno a ser adicionado");
+int codAdd = Int32.Parse(Console.ReadLine());
+if (!alunos.ContainsKey(codAdd))
+{
+    alunos.Add(codAdd, new Aluno (novoNome, novaNota));
+    Console.WriteLine("Aluno incluído com sucesso.");
+}
+else
+{
+    Console.WriteLine("Código ja existe..");
+}
+
+ExibirColecao(alunos);
+
+var alunosOrdenados = alunos.OrderBy(x => x.Value.Nome);
+
+foreach (var item in alunosOrdenados)
+{
+    Console.WriteLine($"{item.Key} - Nome: {item.Value.Nome} | Nota: {item.Value.Nota}\n");
+}
+
+Console.ReadKey();
+
+static void ExibirColecao(Dictionary<int, Aluno> alunos)
+{
+    foreach (var aluno in alunos)
+    {
+        Console.WriteLine($"{aluno.Key} - Nome: {aluno.Value.Nome} | Nota: {aluno.Value.Nota}\n");
+    }
+}
+
+alunos.Clear();
+Console.WriteLine("Fim do exercício.");
+
+public class Aluno
+{
+    public Aluno(string? nome, int nota)
+    {
+        Nome = nome;
+        Nota = nota;
+    }
+    public string? Nome { get; set; }
+    public int Nota { get; set; }
+}
