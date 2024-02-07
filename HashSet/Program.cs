8838b1cf4e203e1586d486cc2ac494692daf45bc
joaoveasey <jovepedro@gmail.com>
2024-02-07 12:19:35 -0300

    #

---
+++ HashSet/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//criando coleção de hashset com itens
+
+var numeros = new HashSet<int>() { 1, 2, 3, 4, 5 };
+
+var frutas = new HashSet<string>() { "uva", "banana", "pera" };
+
+HashSet<int> pares = new HashSet<int>();
+
+for (int i = 0; i < 6; i++)
+{
+    pares.Add(i* 2);
+}
+
+//construtor usando IEnumerable
+var list1 = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+var conjunto = new HashSet<int>(list1);
+
+var list2 = new List<string> { "uva", "banana", "pera" };
+HashSet<string> minhasFrutas = new HashSet<string>(list2);
+
+var impares = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+HashSet<int> meuHashSet = new HashSet<int>(impares);
+
+Console.ReadKey();
---
+++ SortedDictionary/Program.cs
@@ -0,0 +1,45 @@
+﻿//criando um sort dictionary
+var brics = new SortedDictionary<int, string>()
+{
+    {20, "Brasil" },
+    {10, "China" },
+    {40, "India" },
+    {30, "URSS" }
+};
+
+//add um novo elemento
+if (brics.ContainsKey(70))
+{
+    Console.WriteLine("Chave já existe");
+}
+else
+{
+    brics.Add(70, "Africa");
+}
+
+//qtd de elementos
+Console.WriteLine(brics.Count());
+
+//acessando elemento pela chave 
+Console.WriteLine(brics[20]);
+
+//removendo elemento pela chave
+brics.Remove(10);
+
+//verificando se um valor existe
+Console.WriteLine(brics.ContainsValue("URSS")); //--> true ou false
+
+//obtendo o valor de uma chave
+if(brics.TryGetValue(30, out string? valor))
+{
+    Console.WriteLine(valor);
+}
+else Console.WriteLine("chave não existe");
+
+//percorrendo a coleção
+foreach(var pais in brics)
+{
+    Console.WriteLine($"{pais.Key} - {pais.Value}");
+}
+
+Console.ReadKey();
